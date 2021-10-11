667354ba7ff30d8cfd84029fb594cd82b4c69ebc
Cody Beinert <cody@gamejobs.ninja>
2021-10-11 09:14:22 -0400

    finish the RedBirb implementation

--- AngryBirbs-Unity/Assets/AngryBirbs/Scripts/Components/Birb/AirSpecialBounce.cs
+++ AngryBirbs-Unity/Assets/AngryBirbs/Scripts/Components/Birb/AirSpecialBounce.cs
@@ -4,18 +4,15 @@ public class AirSpecialBounce : MonoBehaviour, IAirSpecial
 {
     [Range( 0, 1 )]
     public float SlowDownFactor = 1;
-    float xSpeed;
+    
 
     public void ExecuteAirSpecial()
     {
-         
         Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
-        xSpeed = rb.velocity.x; 
-        
-
+ 
         rb.AddForce(Vector2.up * -rb.velocity.y*2, ForceMode2D.Impulse);
-        xSpeed = xSpeed * SlowDownFactor;
-
+        rb.AddForce(Vector2.left * (rb.velocity.x * SlowDownFactor), ForceMode2D.Impulse);
+        
         throw new System.NotImplementedException();
     }
 }
